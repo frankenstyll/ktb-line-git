ac4c8449d1b79bee5be73a383ec8db828529364f
newkumpon <nontapap.th@gmail.com>
2019-10-21 15:29:52 +0700

    new project

---
+++ src/main/java/com/ktb/linenoti/ktblinenoti/KtbLineNotiApplication.java
@@ -0,0 +1,32 @@
+package com.ktb.linenoti.ktblinenoti;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@SpringBootApplication
+@LineMessageHandler
+public class KtbLineNotiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbLineNotiApplication.class, args);
+	}
+	
+	@EventMapping
+	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+		System.out.println("[KtbLineNotiApplication][START]handleTextMessage");
+		System.out.println("[KtbLineNotiApplication]" + e);
+        TextMessageContent message = e.getMessage();
+        System.out.println("[KtbLineNotiApplication][END]handleTextMessage");
+        return new TextMessage(message.getText());
+	}
+}
